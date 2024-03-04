9a973792c1d4b648df4fe0d4743101908a3ed364
sofyiq <150921026+sofyiq@users.noreply.github.com>
2024-03-05 00:36:53 +0300

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -26,14 +26,14 @@ ft = f"To use this bot you've to join @{fs}."
 
 batch = []
 
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/cancel'))
+@Drone.on(events.NewMessage(incoming=True, pattern='/cancel'))
 async def cancel(event):
     if not event.sender_id in batch:
         return await event.reply("No batch active.")
     batch.clear()
     await event.reply("Done.")
     
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/batch'))
+@Drone.on(events.NewMessage(incoming=True, pattern='/batch'))
 async def _batch(event):
     if not event.is_private:
         return
